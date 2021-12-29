7a85f5c596cae0eaaa3e299d1efd563875276988
Bkitchingweb <kitching.ben@gmail.com>
2021-12-29 19:19:29 +0000

    router setup

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { firebase, FieldValue } from './lib/firebase';
 import FirebaseContext from './context/firebase';
+import App from './App'
 
 ReactDOM.render(
     <FirebaseContext.Provider value={{ firebase, FieldValue }}>
-        <h1 className="text-red-500">Hello, World</h1>
+        <App/>
     </FirebaseContext.Provider>,
     document.getElementById('root')
 );
